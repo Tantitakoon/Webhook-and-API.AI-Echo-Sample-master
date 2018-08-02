f566fe27019e898c4697ed6f816980d443ed5ab3
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-02 21:43:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 
 const express = require("express");
 const restService = express();
@@ -10,6 +10,25 @@ this.app.intent('Your Color Intent', (conv) => {
           //  conv.close('สวัสดีครับ' + userColor+'คือ '+userColor.length);
 });
 
+
+restService.listen(process.env.PORT || 3000, function() {
+  console.log("Server up and listening");
+});
+*/
+
+'use strict';
+const express = require("express");
+const restService = express();
+// Import the Dialogflow module from the Actions on Google client library.
+const {dialogflow} = require('actions-on-google');
+// Instantiate the Dialogflow client.
+const app = dialogflow({debug: true});
+
+
+app.intent('Your Color Intent', conv => {
+    const userColor = conv.parameters.color;
+    conv.close('เลขนำโชคสำหรับ' + userColor+'คือ '+userColor.length);
+});
 
 restService.listen(process.env.PORT || 3000, function() {
   console.log("Server up and listening");
