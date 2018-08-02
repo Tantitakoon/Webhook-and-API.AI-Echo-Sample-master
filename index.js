907958428465192ced61010d6c26fd46d578e6ef
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-02 07:11:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require("express");
 const restService = express();
-/*var dialog = require('actions-on-google');
+var dialog = require('actions-on-google');
 this.app = (0,dialog.dialogflow)();
 this.app.intent('Your Color Intent', (conv) => {
         //const userColor = conv.parameters.color;
          conv.close("BYEBYE " ); 
           //  conv.close('สวัสดีครับ' + userColor+'คือ '+userColor.length);
-});*/
+});
 
 
 restService.listen(process.env.PORT || 3000, function() {
