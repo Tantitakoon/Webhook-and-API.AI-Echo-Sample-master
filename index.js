7ce605456db1a86a69a8b62d1141c800ab219376
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-02 02:47:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,200 +1,109 @@
-"use strict";
+// [START app]
+'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
+process.env.DEBUG = 'actions-on-google:*';
+let ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
+let sprintf = require('sprintf-js').sprintf;
 
-const restService = express();
+// The context and actions as we declare them in api.ai
+const QUIZ_CONTEXT = 'quiz';
+const GENERATE_QUESTION_ACTION = 'generate_question';
+const CHECK_ANSWER_ACTION = 'check_answer';
+const QUIT_ACTION = 'quit';
+const DEFAULT_FALLBACK_ACTION = 'input.unknown';
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+// String format to use as line response
+const PLUS_QUESTION = '%s plus %s';
+const WRONG_ANSWER = 'No, it\'s not %s. Try again.';
+const CORRECT_ANSWER = 'Correct! How much is %s?';
+const WELCOME_MESSAGE = 'Welcome to the math trainer!';
+const ASK_QUESTION = 'How much is %s';
+const QUIT_MESSAGE = 'See you later.';
+const FALLBACK_MESSAGE = 'HMMM I didn\'t get that, I asked how much is %s';
 
-restService.use(bodyParser.json());
+const min = 0;
+const max = 10;
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
+exports.math_trainer = function (request, response) {
+    // print the request to the log
+    console.log('headers: ' + JSON.stringify(request.headers));
+    console.log('body: ' + JSON.stringify(request.body));
 
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music five":
-      speech =
-        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
-      break;
-    case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
-    case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
-    case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
-    case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
-    case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
+    const assistant = new ApiAiAssistant({request: request, response: response});
 
-restService.post("/video", function(req, res) {
-  return res.json({
-    speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/slack-test", function(req, res) {
-  var slack_message = {
-    text: "Details of JIRA board for Browse and Commerce",
-    attachments: [
-      {
-        title: "JIRA Board",
-        title_link: "http://www.google.com",
-        color: "#36a64f",
-
-        fields: [
-          {
-            title: "Epic Count",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Story Count",
-            value: "40",
-            short: "false"
-          }
-        ],
-
-        thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-      },
-      {
-        title: "Story status count",
-        title_link: "http://www.google.com",
-        color: "#f49e42",
-
-        fields: [
-          {
-            title: "Not started",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
-  };
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
+    function generateQuestion (assistant) {
+        console.log('generateQuestion');
+        let newQuestion = getNextQuestion();
+        assistant.data.answer = newQuestion.answer;
+        assistant.data.question = newQuestion.question;
+        assistant.setContext(QUIZ_CONTEXT);
+        assistant.ask(printf(WELCOME_MESSAGE + ' ' +
+            ASK_QUESTION, newQuestion.question));
     }
-  });
-});
 
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
-});
+    function checkAnswer (assistant) {
+        console.log('checkAnswer');
+        let answer = assistant.data.answer;
+        let userAnswer = assistant.getArgument("number") ? parseInt(assistant.getArgument("number")) : '';
+
+        if (answer != userAnswer) {
+            assistant.ask( printf(WRONG_ANSWER, userAnswer));
+        } else {
+            let newQuestion = getNextQuestion();
+            assistant.data.answer = newQuestion.answer;
+            assistant.data.question = newQuestion.question;
+            assistant.ask(printf(CORRECT_ANSWER, newQuestion.question));
+        }
+    }
+
+    function defaultFallback (assistant) {
+        console.log('defaultFallback: ' + assistant.data.fallbackCount);
+        if (assistant.data.fallbackCount === undefined) {
+            assistant.data.fallbackCount = 0;
+        }
+        assistant.data.fallbackCount++;
+        assistant.ask(printf(FALLBACK_MESSAGE, assistant.data.question));
+    }
+
+    /**
+     * Use Tell to send goodbye message and close the mic
+     * @param assistant
+     */
+    function quit (assistant) {
+        console.log('quit');
+        assistant.tell(QUIT_MESSAGE);
+    }
+
+    /**
+     * Use sprintf to reformat the string and add params to it
+     * @param line
+     * @returns {*}
+     */
+    function printf(line) {
+        console.log('printf: ' + line);
+        return sprintf.apply(this, arguments);
+    }
+
+    // Map all the actions that create on api.ai to the function in this file
+    let actionsMap = new Map();
+    actionsMap.set(GENERATE_QUESTION_ACTION, generateQuestion);
+    actionsMap.set(CHECK_ANSWER_ACTION, checkAnswer);
+    actionsMap.set(DEFAULT_FALLBACK_ACTION, defaultFallback);
+    actionsMap.set(QUIT_ACTION, quit);
+    assistant.handleRequest(actionsMap);
+};
+
+/**
+ * Randomize the next question
+ * @returns {{answer: number, question: string}}
+ */
+function getNextQuestion (){
+    let value1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    let value2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    let res = {
+        answer: (value1 + value2),
+        question: sprintf(PLUS_QUESTION, value1, value2)
+    };
+    console.log(JSON.stringify(res));
+    return res;
+}
