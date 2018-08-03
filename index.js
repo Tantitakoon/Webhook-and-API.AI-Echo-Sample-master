ab567ee32f414e6505c89238b443e5e2fd30a915
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-03 04:03:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { dialogflow } = require('actions-on-google');
 
-const app = dialogflow();
+const app = dialogflow({ debug: true })
 
-app.intent('Your Color Intent', (conv) => {
-           const userColor = conv.parameters.color;
-
-               conv.close('สวัสดีครับ' + userColor+'คือ '+userColor.length);
+app.intent('Your Color Intent', conv => {
+          conv.close('Thanks for talking to me!');
    });
 
 express().use(bodyParser.json(), app).listen(process.env.PORT || 3000);
