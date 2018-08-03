dbc90d0dac4f374bf5cbbd80f449c91514acff7f
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-03 03:10:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,17 @@ const app = dialogflow()
 
 express().use(bodyParser.json(), app).listen(3000)*/
 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { dialogflow } = require('actions-on-google');
 
 const app = dialogflow();
 
-// fulfillment code here
+app.intent('Your Color Intent', (conv) => {
+           const userColor = conv.parameters.color;
+
+               conv.close('สวัสดีครับ' + userColor+'คือ '+userColor.length);
+   });
 
 express().use(bodyParser.json(), app).listen(process.env.PORT || 3000);
