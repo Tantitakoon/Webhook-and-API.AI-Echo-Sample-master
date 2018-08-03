ce1b49d561612283175dbc4076c0286ca2368e23
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-03 04:05:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const { dialogflow } = require('actions-on-google');
 
 const app = dialogflow({ debug: true })
-
+const router = express.Router()
+router.use(app)
 app.intent('Your Color Intent', conv => {
           conv.close('Thanks for talking to me!');
    });
