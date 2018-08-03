7ed0bf6be6d7895e7aafba1fbaf7b6f4e02ebb53
Tantitakoon <35595762+Tantitakoon@users.noreply.github.com>
2018-08-03 02:18:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 'use strict';
-
+const express = require("express");
+const bodyParser = require('body-parser')
 const {dialogflow} = require('actions-on-google');
 
 
@@ -9,5 +10,13 @@ const app = dialogflow({debug: true});
 app.intent('Your Color Intent', (conv) => {
      conv.ask('YoUUUUUUUUUUUUUUUUUUUUUUUUUUUUU');
 });
+express().use(bodyParser.json(), app).listen(3000)
+/*exports.yourAction = functions.https.onRequest(app);
 
-exports.yourAction = functions.https.onRequest(app);
+const express = require('express')
+const bodyParser = require('body-parser')
+const { dialogflow } = require('actions-on-google')
+
+const app = dialogflow()
+
+express().use(bodyParser.json(), app).listen(3000)/*
